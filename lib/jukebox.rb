893ab69435ddb09ce521ef5dd058c1c927fe9147
Meredith McIntosh <meredith@mcintosh.io>
2020-11-15 20:41:36 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,12 +1,4 @@
 require "pry"
-# def say_hello(name)
-#   "Hi #{name}!"
-# end
- 
-# puts "Enter your name:"
-# users_name = gets.strip
- 
-# puts say_hello(users_name)
 
 songs = [
   "Phoenix - 1901",
