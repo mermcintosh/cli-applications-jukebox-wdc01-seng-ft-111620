0e56864ab24ace15de2c3b4d7ffbfbc311692005
Meredith McIntosh <meredith@mcintosh.io>
2020-11-15 20:38:45 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -28,11 +28,11 @@ def help
   puts "- exit : exits this program"
 end
 
-# def list(my_songs)
-#   my_songs.each_with_index do |song, index|
-#     puts "#{index+1}. #{song}"
-#   end
-# end
+def list(my_songs)
+  my_songs.each_with_index do |song, index|
+    puts "#{index+1}. #{song}"
+  end
+end
 
 # def play(my_songs)
 #   puts "Please enter a song name or number:"
